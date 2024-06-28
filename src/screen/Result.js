c35e083609772934d3c79ca1719932a985fa5ee8
TuanDat01 <tuandatt2003@gmail.com>
2024-06-28 10:30:55 +0700

    upadte_share_1

--- src/screen/Result.js
+++ src/screen/Result.js
@@ -1,17 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import questionsData from '../data.json';
 import level1Image from '../image/level1.jpg';
 import level2Image from '../image/level2.jpg';
 import level3Image from '../image/level3.jpg';
 import level4Image from '../image/level4.jpg';
 import level5Image from '../image/level5.jpg';
-import { Helmet } from 'react-helmet';
 import './instruct.css';
-import { useNavigate  } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useEffect,useState } from 'react';
 
 function Result({ score }) {
   const [result, setResult] = useState({});
   const navigate = useNavigate();
+
   useEffect(() => {
     const Find = (score) => {
       let checkResult = questionsData.results.filter(
@@ -21,13 +22,7 @@ function Result({ score }) {
     };
     Find(score);
   }, [score]);
-  useEffect(() => {
-    // Function to set level in global scope
-    window.setGlobalLevel = (level) => {
-      // Access level here and do whatever you need with it
-      console.log('Global level:', level);
-    };
-  }, [result]);
+
   useEffect(() => {
     if (window.FB) {
       window.FB.XFBML.parse();
@@ -35,41 +30,42 @@ function Result({ score }) {
   }, [result]);
 
   return (
-    <div>
-      <Helmet>
-                <meta property="og:title" content="Test App" />
-                <meta property="og:description" content="Đánh giá mức độ trưởng thành về quản trị trải nghiệm khách hàng" />
-                <meta property="og:image" content={
-                    result.level === 1 ? "../image/level1.jpg" :
-                    result.level === 2 ? "../image/level5.jpg" :
-                    result.level === 3 ? "../image/level3.jpg" :
-                    result.level === 4 ? level4Image :
-                                    level5Image
-                } />
-                <meta property="og:url" content="https://test-tool-rho.vercel.app/result" />
-                <meta property="og:type" content="website" />
-                <meta property="fb:app_id" content="1384301575511797" /> {/* Replace with your actual Facebook App ID */}
-            </Helmet>
-      <h3 className="header__title">{questionsData.title}</h3>
-      <div className="main">
-        <div className="main__result">
-          <img
-            className="main__result__image"
-            src="https://assets.filum.ai/assessments/voc-level3.svg"
-            alt="Result"
-          />
-          <div className="main__result__title">
-            <h3>VOICE OF THE CUSTOMER - CẤP ĐỘ {result.level}</h3>
-            <h1>{result.name}</h1>
+    <HelmetProvider>
+      <div>
+        <Helmet>
+          <meta property="og:title" content="Test App" />
+          <meta property="og:description" content="Đánh giá mức độ trưởng thành về quản trị trải nghiệm khách hàng" />
+          <meta property="og:image" content={
+            result.level === 1 ? "https://baobinhdinh.vn/viewimage.aspx?imgid=253630" :
+            result.level === 2 ? level2Image :
+            result.level === 3 ? level3Image :
+            result.level === 4 ? level4Image :
+            level5Image
+          } />
+          <meta property="og:url" content="https://test-tool-rho.vercel.app/result" />
+          <meta property="og:type" content="website" />
+          <meta property="fb:app_id" content="1384301575511797" />
+        </Helmet>
+        <h3 className="header__title">{questionsData.title}</h3>
+        <div className="main">
+          <div className="main__result">
+            <img
+              className="main__result__image"
+              src="https://assets.filum.ai/assessments/voc-level3.svg"
+              alt="Result"
+            />
+            <div className="main__result__title">
+              <h3>VOICE OF THE CUSTOMER - CẤP ĐỘ {result.level}</h3>
+              <h1>{result.name}</h1>
+            </div>
           </div>
+          {result && result.description && <h5>{result.description.text}</h5>}
+          <h2>{score}</h2>
+          <h4>score</h4>
+          <button onClick={() => { navigate("/share", { state: { level: result.level } }) }}>Share</button>
         </div>
-        {result && result.description && <h5>{result.description.text}</h5>}
-        <h2>{score}</h2>
-        <h4>score</h4>
-        
-        <button onClick={()=>{navigate("/share",{state:{level:result.level}})}}>Share</button>
       </div>
-    </div>
+    </HelmetProvider>
   );
 }
 
